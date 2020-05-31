88765d7986d097a65494811bc8311d9a967fda5b
yuke wang <yuke_wang@ucsb.edu>
2020-05-31 11:21:41 -0700

    update gcn model

--- pywrapper/pytorch-version/model.py
+++ pywrapper/pytorch-version/model.py
@@ -1,4 +1,4 @@
-# import SAG_kernel
+from gcn import GCN_SAG
 import torch.nn.functional as F
 import torch
 
@@ -8,37 +8,30 @@ class GCN(object):
         self.n_hidden = n_hidden
         self.n_class = n_class
         self.n_hidden_layers = n_hidden_layers
-        # self.aggre = SAG_kernel()
         self.head_update = torch.nn.Linear(n_feature, n_hidden, bias=True)
         self.hidden_update = torch.nn.Linear(n_hidden, n_hidden, bias=True)
         self.tail_update = torch.nn.Linear(n_hidden, n_class, bias=True)
-        
+        self.gcn_aggre = GCN_SAG.apply
 
     def forward(self, graph_obj):
 
         numGroups, nodePointer, n_hidden, \
             numNodes, groupNodePointer, edgeList, embed = graph_obj
 
-        # out = self.head_update(feature_embedding)
-        # x = torch.zeros((numNodes, n_hidden))
-        # self.aggre(numGroups, nodePointer, n_hidden,
-        #            numNodes, groupNodePointer, edgeList,
-        #             x, out)
-
-        print(embed.size())
-        # print(self.head_update)        
         x = self.head_update(embed)
+        self.aggre(nodePointer, edgeList, groupNodePointer, x,
+                     numGroups, n_hidden, numNodes)
 
-        x = F.relu(x)
         print(x.size())
+        # print(self.head_update)        
+        # x = self.head_update(embed)
+        x = F.relu(x)
+        # print(x.size())
 
         for hid in range(self.n_hidden_layers - 1):
-            # out = self.hidden_update(x)
-            # x = torch.zeros((numNodes, n_hidden))
-            # self.aggre(numGroups, nodePointer, n_hidden,
-            #           numNodes, groupNodePointer, edgeList,
-            #            x, out)
             x = self.hidden_update(x)
+            self.aggre(nodePointer, edgeList, groupNodePointer, 
+                        x, numGroups, n_hidden, numNodes)
             x = F.relu(x)
         
         x = self.tail_update(x)
